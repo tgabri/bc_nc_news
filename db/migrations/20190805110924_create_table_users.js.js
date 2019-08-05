38eda85e92feb75e28d09aeafb7e733099ffa3b3
Gabor <tgabri@gmail.com>
2019-08-05 12:03:46 +0100

    finished migrations

---
+++ db/migrations/20190805110924_create_table_users.js.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  console.log('creating users table...');
+  return knex.schema.createTable('users', usersTable => {
+    usersTable.string('username').primary();
+    usersTable.string('avatar_url');
+    usersTable.string('name');
+  });
+};
+
+exports.down = function(knex) {
+  console.log('removing users table...');
+  return knex.schema.dropTable('topics');
+};
