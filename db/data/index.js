1e4cee650f7878ad022a591353305d33964a47d2
Gabor <tgabri@gmail.com>
2019-08-05 10:40:00 +0100

    created index files

---
+++ db/data/index.js
@@ -0,0 +1,10 @@
+const ENV = process.env.NODE_ENV || 'development';
+
+const testData = require('./test-data');
+const devData = require('./development-data');
+
+const data = {
+  test: testData,
+  development: devData
+};
+module.exports = data[ENV];
